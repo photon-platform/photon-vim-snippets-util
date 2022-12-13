39b935899a45bdc2f7cd6eefa59ebce6a3248b76
phiarchitect <phi@phiarchitect.com>
2022-12-12 18:50:21 -0800

    fix complete function

--- pythonx/vimsnippets.py
+++ pythonx/vimsnippets.py
@@ -15,15 +15,16 @@ def complete(tab, opts):
 
     :return: a string that match with tab
     """
-    el = [x for x in tab]
-    pat = "".join(list(map(lambda x: x + "\w*" if re.match("\w", x) else x,
-                           el)))
-    try:
-        opts = [x for x in opts if re.search(pat, x, re.IGNORECASE)]
-    except:
-        opts = [x for x in opts if x.startswith(tab)]
-    if not len(opts) or str.lower(tab) in list(map(str.lower, opts)):
-        return ""
+    #  el = [x for x in tab]
+    #  pat = "".join(list(map(lambda x: x + "\w*" if re.match("\w", x) else x,
+                           #  el)))
+    #  try:
+        #  opts = [x for x in opts if re.search(pat, x, re.IGNORECASE)]
+    #  except:
+    opts = [x[len(tab):] for x in opts if x.startswith(tab)]
+    #  if not len(opts) or str.lower(tab) in list(map(str.lower, opts)):
+    if len(opts) == 1:
+        return opts[0]
     cads = "|".join(opts[:5])
     if len(opts) > 5: cads += "|..."
     return "({0})".format(cads)
